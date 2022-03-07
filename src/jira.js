b172c4a50763b7ea9f079297b2874946b7820da2
dimitris-flyr <99741372+dimitris-flyr@users.noreply.github.com>
2022-03-07 10:41:23 +0100

    init commit

---
+++ src/jira.js
@@ -0,0 +1,32 @@
+(async function() {
+    // Board page view
+    const queryMatches = window.location.search.match(/selectedIssue=(\w+-\d+)/);
+    // Main ticket page
+    const pathnameMatches = window.location.pathname.match(/browse\/(\w+-\d+)/);
+    const ticketID = queryMatches ? queryMatches[1] : pathnameMatches ? pathnameMatches[1] : "";
+    if (!ticketID) {
+        console.warn("No Jira ticket found in this page");
+        return;
+    }
+
+    const issue = await jQuery.getJSON('/rest/api/3/issue/' + ticketID);
+    const jiraTicketTitle = issue.fields.summary;
+    const jiraTicketType = issue.fields.issuetype.name;
+    
+    const ticketToGithubTitle = {
+        "bug": "fix",
+        // add any other special mapping Jira to GitHub title here, apart from featrures
+    };
+    const githubPRType = ticketToGithubTitle?.[jiraTicketType] || "feat";
+    const githubPRTitle =  `${githubPRType}: ${ticketID} ${jiraTicketTitle}`;
+    
+    const ticketToGithubBranch = {
+        "bug": "hotfix",
+        // add any other special mapping Jira to GitHub branch type here, apart from featrures
+    };
+    const githubBranchType = ticketToGithubBranch?.[jiraTicketType] || "feature";
+    const githubBranch = `${githubBranchType}/${ticketID}-${jiraTicketTitle.toLowerCase().replace(/\s/g, "-")}`;
+    
+    prompt("Copy your branch name:", githubBranch);
+    prompt("Copy your PR title:", githubPRTitle);
+})()
